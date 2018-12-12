b06cdd3d900b8cccf122aad245e60ddcc2ec1a83
robin <y8765gd@gmail.com>
2018-12-12 18:21:41 +0800

    first command

---
+++ README.md
@@ -0,0 +1,3 @@
+
+## Enviroments
+- `conda create -n py34 python=3.4 anaconda`
---
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,27 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        url = 'http://quotes.toscrape.com/'
+        tag = getattr(self, 'tag', None)
+        self.logger.info("testing!!!!")
+        if tag is not None:
+            url = url + 'tag/' + tag
+        self.logger.info("url: {}".format(url))
+        yield scrapy.Request(url, self.parse)
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            self.logger.info("quote: {}".format(quote))
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('small.author::text').extract_first(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
+        
