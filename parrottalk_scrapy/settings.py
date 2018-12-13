f381b737cbc9cf67dc1cf2eff318b278025f73d4
robin <y8765gd@gmail.com>
2018-12-13 11:08:48 +0800

    change structure

--- parrottalk_scrapy/settings.py
+++ parrottalk_scrapy/settings.py
@@ -9,10 +9,10 @@
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'tutorial'
+BOT_NAME = 'parrottalk_scrapy'
 
-SPIDER_MODULES = ['tutorial.spiders']
-NEWSPIDER_MODULE = 'tutorial.spiders'
+SPIDER_MODULES = ['parrottalk_scrapy.spiders']
+NEWSPIDER_MODULE = 'parrottalk_scrapy.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
